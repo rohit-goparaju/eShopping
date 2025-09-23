52549dae08993df48b37741f03351c9cb2d3801b
Rohit Goparaju <rohitgoparaju.r96@gmail.com>
2025-09-23 20:11:52 +0530

    update cors config allowed origins

--- src/main/java/com/projects/eShopping/config/CorsConfig.java
+++ src/main/java/com/projects/eShopping/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
 	@Bean
 	public CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration config = new CorsConfiguration();
-		config.setAllowedOriginPatterns(List.of("*"));
+		config.setAllowedOrigins(List.of("http://localhost:5173"));
 		config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 		config.setAllowedHeaders(List.of("*"));
 		
